5678575324604b5218862739ec47a77e36bd0744
Itay Fridman <fitay123@gmail.com>
2019-03-19 20:11:29 +0200

    Add Course, Rating Classes

---
+++ SocialNetwork/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Course
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2-50")]
+        public string name { get; set; }
+        [Required]
+        public float price { get; set; }
+        [Required]
+        public string tags { get; set; }
+        public string ratings { get; set; }
+        public float avgRating { get; set; }
+        public string books { get; set; }
+
+    }
+}
---
+++ SocialNetwork/Models/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Rating
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public int courseId { get; set; }
+        [Required]
+        public int studentId { get; set; }
+        [Required]
+        public int rating { get; set; }
+        [Required]
+        public string comment { get; set; }
+    }
+}
