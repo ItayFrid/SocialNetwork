ac9b153990259a1d0db2db598d3369c76fcf4bec
Idan <idanah24@gmail.com>
2019-03-19 20:17:17 +0200

    Enroll & Teache classes

---
+++ SocialNetwork/Models/Enroll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{   
+
+    //This class holds refrences of teacher, course and students
+    //Instance of this class is created when the first student signs up to the course
+
+    public class Enroll
+    {
+
+        public string teacherId { get; set; }
+
+        public string courseId { get; set; }
+
+        public string studentsId { get; set; }
+
+    }
+}
--- SocialNetwork/Models/User.cs
+++ SocialNetwork/Models/User.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SocialNetwork.Models
-{
+{   
+    //This class describes the basic user
+    //Inherating classes: Admin, Teacher and Student
     public class User
     {
+        //TODO: Add regular expressions
+        [Required]
+        public string name { get; set; }
+
+        [Required]
         [Key]
+        [EmailAddress]
         public string email { get; set; }
 
+        [Required]
         public string password { get; set; }
+
+        [Required]
         public string role { get; set; }
     }
 }
