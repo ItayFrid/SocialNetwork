840804628cc813fdea29a05fb0d21ca54f96f97e
Idan <idanah24@gmail.com>
2019-03-20 15:06:52 +0200

    Student class, Login controller, Encryption, Data Layer

---
+++ SocialNetwork/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Student : User
+    {
+
+        public string courses { get; set; }
+
+        public string ratings { get; set; }
+
+    }
+}
