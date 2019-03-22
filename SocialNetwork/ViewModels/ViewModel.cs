d3df49b797095ab19365fe71a260eb6a5caa2688
ItayFrid <fitay123@gmail.com>
2019-03-22 18:29:25 +0200

    Add ViewModel class Update layout Add UserLogin view

---
+++ SocialNetwork/ViewModels/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.ViewModels
+{
+	public class ViewModel
+	{
+		public Course course { get; set; }
+		public List<Course> courses { get; set; }
+
+		public Enroll enroll { get; set; }
+		public List<Enroll> enrolls { get; set; }
+
+		public Rating rating { get; set; }
+		public List<Rating> ratings { get; set; }
+
+		public Student student { get; set; }
+		public List<Student> students { get; set; }
+
+		public Teacher teacher { get; set; }
+		public List<Teacher> teachers { get; set; }
+
+		public User user { get; set; }
+		public List<User> users { get; set; }
+	}
+}
