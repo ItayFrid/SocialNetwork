f6cc0bf113205cb5d4b99b576b6cdd748971a232
Itay Fridman <fitay123@gmail.com>
2019-04-01 18:48:31 +0300

    Complaint System

---
+++ SocialNetwork/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using SocialNetwork.DAL;
+using SocialNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    [Authorize(Roles ="Student")]
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            DataLayer dal = new DataLayer();
+            List<Student> student = (from x in dal.students
+                                     where x.email == User.Identity.Name
+                                     select x).ToList<Student>();       //Getting student from db
+            return View(student[0]);
+        }
+
+        public ActionResult Complaint()
+        {
+            Complaint complaint = new Complaint();
+            return View(complaint);
+        }
+    }
+}
---
+++ SocialNetwork/Models/Complaint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class Complaint
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string Sender { get; set; }
+        public string Target { get; set; }
+        public string body { get; set; }
+    }
+}
