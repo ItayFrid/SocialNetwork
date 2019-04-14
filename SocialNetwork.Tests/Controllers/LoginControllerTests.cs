26e096e2a7d90451c872106200d2ff0278b26cfe
Itay Fridman <fitay123@gmail.com>
2019-04-14 20:52:06 +0300

    Add UnitTests

---
+++ SocialNetwork.Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork;
+using SocialNetwork.Controllers;
+using System.Web.Mvc;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            // Arrange
+            LoginController controller = new LoginController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod()]
+        public void LoginTest()
+        {
+            //Arange
+            LoginController login = new LoginController();
+            //Act
+            User user = new User {
+                email="fitay123@gmail.com",
+                password = "123",
+                id = "305360653"
+            };
+            ViewResult result = login.Login(user) as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ SocialNetwork.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SocialNetwork.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SocialNetwork.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4259a16-f131-48c2-941e-e9a0603ce7ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
